114068d04176598655682534e3318fbabe9fa5a5
hamzashadeez <hahmad11178@gmail.com>
2022-01-06 05:28:22 -0800

    new commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const menu_btn = document.getElementById("menu_btn");
+const close = document.getElementById("close");
 const mobile_menu = document.getElementById("mobile_menu");
 const links = document.querySelectorAll("links");
 
@@ -6,9 +7,29 @@ const links = document.querySelectorAll("links");
 function menu (){
     if (mobile_menu.style.display == "none") {
         mobile_menu.style.display = "flex";
+        
     } else {
         mobile_menu.style.display = "none";
       } 
 }
-menu_btn.addEventListener("click", menu);
+
+function openNav(){
+    mobile_menu.style.display = "flex";
+    menu_btn.style.display = "none";
+    close.style.display = "flex";
+    
+
+}
+
+
+function closeNav(){
+    mobile_menu.style.display = "none";
+    menu_btn.style.display = "flex";
+    close.style.display = "none";
+    
+}
+
+
+menu_btn.addEventListener("click", openNav);
+close.addEventListener("click", closeNav);
 
