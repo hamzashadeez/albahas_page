d627e6a5bdcc4418f5ab954268d84f207bf2ccd1
hamzashadeez <hahmad11178@gmail.com>
2022-01-06 06:03:20 -0800

    footer

--- script.js
+++ script.js
@@ -3,33 +3,23 @@ const close = document.getElementById("close");
 const mobile_menu = document.getElementById("mobile_menu");
 const links = document.querySelectorAll("links");
 
-
-function menu (){
-    if (mobile_menu.style.display == "none") {
-        mobile_menu.style.display = "flex";
-        
-    } else {
-        mobile_menu.style.display = "none";
-      } 
+function menu() {
+  close.style.display = "flex";
+  mobile_menu.style.display = "none";
+  menu_btn.style.display = "flex";
 }
 
-function openNav(){
-    mobile_menu.style.display = "flex";
-    menu_btn.style.display = "none";
-    close.style.display = "flex";
-    
-
+function openNav() {
+  mobile_menu.style.display = "flex";
+  menu_btn.style.display = "none";
+  close.style.display = "flex";
 }
 
-
-function closeNav(){
-    mobile_menu.style.display = "none";
-    menu_btn.style.display = "flex";
-    close.style.display = "none";
-    
+function closeNav() {
+  mobile_menu.style.display = "none";
+  menu_btn.style.display = "flex";
+  close.style.display = "none";
 }
-
 
 menu_btn.addEventListener("click", openNav);
 close.addEventListener("click", closeNav);
-
